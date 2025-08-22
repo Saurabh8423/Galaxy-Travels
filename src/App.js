f1bf2c9b80cef97dbc07066fdc2c9dffd4572c35
saurabh8423 <saurabhch9369@gmail.com>
2025-08-22 21:56:19 +0530

    created some folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import Home from './Pages/Home/Home.js';
+import Contact from './Pages/Contact/Contact.js';
+import Training from './Pages/Training/Training.js';
+import Pricing from './Pages/Pricing/Pricing.js';
 import './App.css';
 
+import { BrowserRouter as Router , Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className='app'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/training' element={<Training />} />
+          <Route path='/pricing' element={<Pricing />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
