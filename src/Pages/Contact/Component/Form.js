9002f4d0a529a486607081555f28c2676d463279
saurabh8423 <saurabhch9369@gmail.com>
2025-08-24 21:29:28 +0530

    updated some css in Contact form pages

--- src/Pages/Contact/Component/Form.js
+++ src/Pages/Contact/Component/Form.js
@@ -6,21 +6,23 @@ const Form = () => {
     <h1 className='heading'>Contact Us</h1>
    <section className='container'>
      <form>
-        <div className='field'>
-            <p>Name</p>
-            <input type="text" />
-        </div>
-        <div className='field'>
-            <p>Email</p>
-            <input type="email" />
-        </div>
-        <div className='field'>
-            <p>Phone Number</p>
-            <input type="tel" />
-        </div>
+          <div className='field'>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" placeholder="Enter your name" />
+          </div>
+
+          <div className='field'>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Enter your email" />
+          </div>
+
+          <div className='field'>
+            <label htmlFor="phone">Phone Number</label>
+            <input type="tel" id="phone" placeholder="Enter your phone number" />
+          </div>
 
         <div className="form-button">
-            <button>Submit</button>
+            <button type='submit'>Submit</button>
         </div>
     </form>
    </section>
@@ -29,4 +31,4 @@ const Form = () => {
   )
 }
 
-export default Form
+export default Form;
